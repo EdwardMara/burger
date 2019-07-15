2140812899eb03eb1dc0ede8ee67eb3d36d6379e
Edward Mara <e.james.mara@gmail.com>
2019-07-14 22:35:22 -0700

    created routes for all functions

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,31 @@ var router = express.Router();
 var burger = require("../models/burger");
 
 // Create all our routes and set up logic within those routes where required.
+router.get('/', function(req, res){
+    res.redirect('/index');
+});
 
+router.get('/index', function (req, res){
+    burger.selectAll(function(data){
+        var burgerObject = {
+            burgers:data
+        };
+        res.render('index', burgerObject);
+    });
+});
+
+router.post('/burger/create', function(req, res){
+    burger.insertOne(req.body.burger_name, function(){
+        res.redirect('/index');
+    });
+});
+
+router.post('/burger/eat/:id', function(req,res){
+    burger.updateOne(req.params.id, function(){
+        res.redirect('/index');
+
+    });
+});
 
 
 
