ae894ea410c87d3598ca215d8d29bee62568b04f
Edward Mara <e.james.mara@gmail.com>
2019-07-14 22:25:34 -0700

    orm added

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,30 @@
 var connection = require("./connection");
 
 //use selectAll(), insertOne(), and updateOne()
+var orm = {
+    selectAll: function(cb){
+        connection.query ('SELECT * FROM burgers', function(err, res){
+            if (err) throw err;
+            cb(result);
 
+        });
+    },
+
+    insertOne: function(burger_name, cb){
+        connection.query('INSERT INTO burgers SET ?',{burger_name: burger_name, devoured: false},
+        function(err,result){
+            if(err) throw err;
+            cb(result);
+        });
+    },
+    
+    updateOne: function(burgerID, callback){
+        connection.query('UPDATE burgers SET ? WHERE ?', [{devoured: true}, {id:burgerID}],
+        function(err,result){
+            if (err) throw err;
+            cb(result);
+        });
+    }
+};
 
 module.exports = orm;
